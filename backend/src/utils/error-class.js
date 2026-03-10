ba9a7fb4dd3ff7583c4c76ef1f97c9b50c72b910
Chandanahk6804 <chiruhk6004@gmail.com>
2026-03-10 22:36:36 +0530

    feat: created error-class.js to handle errors

---
+++ backend/src/utils/error-class.js
@@ -0,0 +1,34 @@
+class AppError extends Error{
+    constructor(status, name, message) {
+        super(message)
+        this.status = status
+        this.name = name
+        this.message = message
+        Error.captureStackTrace(this, this.constructor)
+    }
+}
+
+class NotFoundError extends AppError{
+    constructor(name = 'NOT_FOUND', message = 'Resource not found') {
+        super(404, name, message)
+    }
+}
+
+class BadRequestError extends AppError {
+    constructor(name = 'BAD_REQUEST', message = 'Invalid request data' ) {
+        super(400, name, message)
+    }
+}
+
+class ForbiddenError extends AppError {
+    constructor(name = 'FORBIDDEN_ACCESS', message = 'Access denied') {
+        super(403, name, message)
+    }
+}
+
+module.exports = {
+    AppError,
+    NotFoundError,
+    BadRequestError,
+    ForbiddenError
+}
