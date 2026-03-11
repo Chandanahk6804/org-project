a29ba4192a1406b87a01abf455afe9eb9cb015ad
Chandanahk6804 <chiruhk6004@gmail.com>
2026-03-11 21:40:04 +0530

    feat: update get-users.js to send custom response and handle errors

--- backend/src/modules/user/controller/get-users.js
+++ backend/src/modules/user/controller/get-users.js
@@ -1,15 +1,33 @@
 const executeQuery = require("../../../config/db-connection")
+const { AppError } = require("../../../utils/error-class")
+const responseHandler = require("../../../utils/response-handler")
 
 const getUsersQuery = `
     SELECT 
         user_name,
-        user_email,
+        email,
         phone
     FROM user`
 
 
 const getAllUsers = async (req, res) => {
-    const result = await executeQuery(getUsersQuery)
+    const results = await executeQuery(getUsersQuery)
+    
+    if(results.length === 0 || !results) {
+        throw new AppError(
+            false,
+            'UserNotFound',
+            'Users not found'
+        )
+    }
+
+    return responseHandler(
+        res,
+        true,
+        200,
+        "List of users",
+        results
+    )
 }
 
 module.exports = getAllUsers
