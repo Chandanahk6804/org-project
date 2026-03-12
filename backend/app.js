6e90bcfa1016a703dcf21d9c630e1e065ccc6d18
Chandanahk6804 <chiruhk6004@gmail.com>
2026-03-12 22:38:43 +0530

    feat: update app.js to include request body parser

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const errorHandler = require('./src/middleware/error-handler')
 require('dotenv/config')
+
+const userRouter = require('./src/routes/user-routes')
 
 const app = express()
 
+app.use(express.json())
+
+app.use('/users', userRouter)
+
+app.use(errorHandler)
 
 app.listen(
     process.env.PORT, 
