4c31c45ffd4acf4687063b968be41269b988f0c6
Chandanahk6804 <chiruhk6004@gmail.com>
2026-03-16 22:40:54 +0530

    feat: modify jwt-helper.js

--- backend/src/utils/jwt-helper.js
+++ backend/src/utils/jwt-helper.js
@@ -3,10 +3,7 @@ const jwt = require('jsonwebtoken')
 const generateToken = (payload) => {
     return jwt.sign(
         payload,
-        process.env.JWT_SECRET,
-        {
-            expiresIn: '1m'
-        }
+        process.env.JWT_SECRET
     )
 }
 
