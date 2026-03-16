1cc749f7f090befbfabbabe2220fc68500dd0a6e
Chandanahk6804 <chiruhk6004@gmail.com>
2026-03-16 22:41:36 +0530

    feat: update app.js to import task and org routers

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,16 @@ require('dotenv/config')
 const errorHandler = require('./src/middleware/error-handler')
 
 const userRouter = require('./src/routes/user-routes')
+const orgRouter = require('./src/routes/org-routes')
+const taskRouter = require('./src/routes/task-routes')
 
 const app = express()
 
 app.use(express.json())
 
 app.use('/users', userRouter)
+app.use('/orgs', orgRouter)
+app.use('/tasks', taskRouter)
 
 app.use(errorHandler)
 
